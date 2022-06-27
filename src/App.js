30d8ebcdba9c60ba7d10648a74407ac859b3034c
Austin Gae <austingae@gmail.com>
2022-06-27 13:08:50 -0700

    Completed the Mobile Version of the About Me and Projects Section. Now, I need to work on the Contact Me Section. After that, I need to work on making my portfolio website responsive to the screen size.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+
+import profileImage from './Images/profile-image.jpeg';
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+
+import myResume from './PDF/Resume.pdf';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className='about-me'>
+            <h1 className='about-me__my-name'>Hello! <br></br>I am Austin Gae.</h1>
+            <img className='about-me__profile-image' src={profileImage}/>
+            <p className='about-me__my-short-description'>Eiusmod ipsum veniam laborum veniam quis minim consequat sit consectetur nisi eu exercitation adipisicing. Voluptate veniam magna nostrud commodo minim ipsum officia exercitation ad voluptate. Nulla duis mollit laborum excepteur eiusmod ea nulla sit. Labore duis eu exercitation sunt velit aliqua occaecat ullamco dolore velit aliqua laborum.</p>
+            <div className='about-me__social-media-container'> 
+              <a href='https://www.linkedin.com/in/austin-gae/'><FontAwesomeIcon className='about-me__social-media-icon about-me__linkedin' icon={faLinkedin} size="2x"/></a>
+              <a href='https://github.com/austingae'><FontAwesomeIcon className='about-me__social-media-icon about-me__github' icon={faGithub} size="2x"/></a>
+              <a href='mailto:austingae@gmail.com'><FontAwesomeIcon className='about-me__social-media-icon about-me__email' icon={faEnvelope} size="2x"/></a>
+            </div>
+            <div className='about-me__resume-container'>
+              <a href={myResume} target='_blank'><FontAwesomeIcon className='about-me__resume' icon={faFile} size="2x" /></a>
+              <a className='about-me__view-my-resume' href={myResume} target='_blank'>View my Resume</a>
+            </div>
+        </div>
       </header>
-    </div>
+
+      <hr className='line'></hr>
+
+      <main>
+        <div className='my-projects'>
+          <h2 className='my-projects__title'>My Projects</h2>
+          <div className='project'>
+            <h3 className='project__title'>Project Title</h3>
+            <p className='project__description'>Cupidatat Lorem cillum nisi irure aute reprehenderit deserunt est irure in laborum anim ea. Nulla tempor sunt id quis id. Dolore et enim incididunt non adipisicing dolor fugiat tempor ea incididunt eu eiusmod do ex.</p>
+            <div className='project__buttons-container'>
+              <button className='project__button'>View the Website</button>
+              <button className='project__button'>View the Code</button>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+
+
   );
 }
 
 export default App;
+
+/*
+HTML & CSS & Javascript Approach: 
+  1) Mobile First Approach - Yes. 
+  2) Create the HTML layout - Yes. 
+  - 
+  <section>
+    <div class="container">
+    </div>
+  </section>
+  3) Create the HTML elements - Yes. 
+  4) Decorate the HTML elements - 
+  If * {box-sizing: content-box}
+  - Parent Div's Width = Child Div's Width+Padding+Border+Margin
+  - Parent Div's Width = Block Element's Width+Padding+Border+Margin
+  - However, Parent Div's Width != Inline Element's Width+Padding+Border+Margin
+  5) Javascript 
+  */
