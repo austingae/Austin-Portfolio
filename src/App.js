12c53978a3bb47ae4f43a08cf83bb2b4793b6758
Austin Gae <austingae@gmail.com>
2022-07-19 16:09:17 -0700

    fixing error

--- src/App.js
+++ src/App.js
@@ -56,21 +56,3 @@ function App() {
 }
 
 export default App;
-
-/*
-HTML & CSS & Javascript Approach: 
-  1) Mobile First Approach - Yes. 
-  2) Create the HTML layout - Yes. 
-  - 
-  <section>
-    <div class="container">
-    </div>
-  </section>
-  3) Create the HTML elements - Yes. 
-  4) Decorate the HTML elements - Yes. 
-  If * {box-sizing: content-box}
-  - Parent Div's Width = Child Div's Width+Padding+Border+Margin
-  - Parent Div's Width = Block Element's Width+Padding+Border+Margin
-  - However, Parent Div's Width != Inline Element's Width+Padding+Border+Margin
-  5) Javascript - Yes. 
-  */
